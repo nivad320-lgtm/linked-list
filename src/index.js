53444689afb74497c55f27e6f3b180bf61a03ec1
Davin <286156958+nivad320-lgtm@users.noreply.github.com>
2026-07-26 13:23:16 +0900

    Solve linked list questions

--- src/index.js
+++ src/index.js
@@ -1,32 +1,28 @@
-import "./styles.css";
+import { LinkedList } from "./linked-list-test.js";
 
-class LinkedList {
+const list = new LinkedList();
 
-    constructor() {
-        this.list = [] // empty object.
-    }
+console.log(list.list);
 
-    append(value) { // value is a string
-        // Create new Node
-        // Node value = value
-        // if list is empty, create new node with head
-        // new Node (value, 'head')
-        // this.list.append(new Node(value, 'head'))
-        if (this.list.length === 0) {
-            this.list.append(new Node(value, 'head'));
-        }
+list.append('hi')
 
-        // if list is not empty and have a node
-        // tmp.next = new Node(value)
-        // append it to this.list
-    }
-}
+console.log(list.list);
 
-class Node {
-    constructor(value, nextNode) {
-        this.value = value;
-        this.nextNode = nextNode || null;
-    }
-}
+list.append('my')
 
-export {LinkedList, Node}
+console.log(list.list);
+console.log(list.head)
+
+list.prepend('Ola');
+console.log(list.list);
+console.log(list.head)
+
+console.log(list.size())
+console.log(list.head())
+console.log(list.tail())
+console.log(list.at(2));
+console.log(list.contains('my'))
+console.log(list.findIndex('123123my'))
+console.log(list.at(2));
+console.log(list.toString())
+
--- src/linked-list-test.js
+++ src/linked-list-test.js
@@ -1,1 +1,144 @@
-import { LinkedList, Node } from "./index.js";
+
+class LinkedList {
+
+    constructor() {
+        this._head = null;
+    }
+
+    append(value) { // value is a string
+        // Create new Node
+        // Node value = value
+        // if list is empty, create new node with _head
+        // new Node (value, '_head')
+        // this.list.append(new Node(value, '_head'))
+        if (this._head === null) {
+            this.no_headAppend(value);
+        }
+        else {
+            //traversing
+            let temp = this._head; //_head
+            while(temp.next !== null) {
+                temp = temp.next;
+            }
+            temp.next = new Node(value)
+
+        }
+
+
+        // if list is not empty and have a node
+        // tmp.next = new Node(value)
+        // append it to this.list
+    }
+
+    prepend(value) {
+        if (this._head === null) {
+            this.no_headAppend(value);
+        }
+        this._head = new Node(value, this._head)
+
+    }
+
+    no_headAppend(value) {
+        this._head = new Node(value);
+    }
+
+    size () { // return the total number of nodes
+        //traverse
+        if (this._head === null ) {
+            return undefined
+        }
+        let temp = this._head;
+        let size = 1;
+        while (temp.next !== null) {
+            temp = temp.next;
+            size++;
+        }
+        return size;
+        
+    }
+    head() {
+        if (this._head === null) {
+            return undefined
+        }
+        return this._head.value;
+    }
+
+    tail() {
+        if (this._head === null) {
+            return undefined
+        }
+        let temp = this._head;
+        while (temp.next !== null) {
+            temp = temp.next;
+        } return temp.value;
+    }
+    at(index) {
+        if (index === 0 && this._head !== null) {
+            return this._head
+        }
+
+        if (this._head === null || index < 0 || !this.at(index-1)) {
+            return undefined
+        }
+
+        return this.at(index-1).next;
+    }
+
+    pop () {
+        if (this._head === null) {
+            return undefined
+        }
+        let temp = this._head;
+        this._head = temp.next;
+        return temp
+    }
+
+    contains(value){
+        if (this._head === null) {
+            return false
+        }
+        let temp = this._head;
+        while (temp !== null) {
+            if(temp.value === value) {
+                return true
+            }
+            temp = temp.next;
+        } 
+
+        return false;
+    }
+    findIndex(value) {
+        let temp = this._head;
+        let index = 0
+        while (temp !== null) {
+            if(temp.value === value) {
+                return index
+            }
+            temp = temp.next;
+            index++
+        } 
+        return -1;
+    }
+    toString() {
+        let myString = '';
+        if (this._head === null) {
+            return myString
+        }
+        let temp = this._head
+        while (temp !== null) {
+            myString += `( ${temp.value} ) -> `
+            temp = temp.next
+        }
+        myString += `null`
+        return myString
+    }
+}
+
+class Node {
+    constructor(value, next) {
+        this.value = value;
+        this.next = next || null;
+    }
+}
+
+export { LinkedList, Node }
