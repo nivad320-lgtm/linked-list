b8eb9c6f76350d8359516d6695358494bcc9696e
Davin <286156958+nivad320-lgtm@users.noreply.github.com>
2026-07-26 10:59:55 +0900

    Set up js file to test linked list

--- src/index.js
+++ src/index.js
@@ -3,15 +3,30 @@ import "./styles.css";
 class LinkedList {
 
     constructor() {
-
+        this.list = [] // empty object.
     }
 
-    append()
+    append(value) { // value is a string
+        // Create new Node
+        // Node value = value
+        // if list is empty, create new node with head
+        // new Node (value, 'head')
+        // this.list.append(new Node(value, 'head'))
+        if (this.list.length === 0) {
+            this.list.append(new Node(value, 'head'));
+        }
+
+        // if list is not empty and have a node
+        // tmp.next = new Node(value)
+        // append it to this.list
+    }
 }
 
 class Node {
     constructor(value, nextNode) {
         this.value = value;
-        this.nextNode = nextNode;
+        this.nextNode = nextNode || null;
     }
 }
+
+export {LinkedList, Node}
