54a238cadbb525150a3a38d560218cf27737ae4f
Davin <286156958+nivad320-lgtm@users.noreply.github.com>
2026-07-26 10:26:46 +0900

    Declare LinkedList and Node classes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import "./styles.css";
+
+class LinkedList {
+
+    constructor() {
+
+    }
+
+    append()
+}
+
+class Node {
+    constructor(value, nextNode) {
+        this.value = value;
+        this.nextNode = nextNode;
+    }
+}
